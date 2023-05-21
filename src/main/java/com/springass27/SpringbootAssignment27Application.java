3bb890ab4fcf31de1493d207110e1b718ee9788c
38rahul <rahulkumarkausall54321@gmail.com>
2023-05-21 20:39:19 +0530

    Initial commit

---
+++ src/main/java/com/springass27/SpringbootAssignment27Application.java
@@ -0,0 +1,13 @@
+package com.springass27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAssignment27Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAssignment27Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springass27/SpringbootAssignment27ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springass27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAssignment27ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
